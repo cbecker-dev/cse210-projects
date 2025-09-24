baa9f0103e0c643ffdad0bae65d8fdaa28df7787
cbecker-dev <bec16022@byui.edu>
2025-09-23 23:21:23 -0600

    Letter Grade Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+
+        int percentage = int.Parse(valueFromUser);
+
+        string letterGrade;
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (!(letterGrade == "D" || letterGrade == "F"))
+        {
+            Console.WriteLine("Congratulations you have passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Too bad, better luck next time.");
+        }
     }
 }
