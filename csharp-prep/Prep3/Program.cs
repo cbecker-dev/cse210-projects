63b9855bc93865008d7526ab57f0af226e8e00e7
cbecker-dev <bec16022@byui.edu>
2025-09-24 00:30:06 -0600

    Guess the number Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+
+        int g = int.Parse(guess);
+
+        while (g != magicNumber)
+        {
+            if (g < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess! ");
+            guess = Console.ReadLine();
+            g = int.Parse(guess);
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
