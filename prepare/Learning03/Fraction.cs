b67c56b2da4767f61506108dba63a5008fef6a56
cbecker-dev <bec16022@byui.edu>
2025-10-03 19:38:18 -0600

    Step 5

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -11,21 +11,44 @@ public class Fraction
         this.top = 1;
         this.bottom = 1;
     }
-
+    public Fraction(int top)
+    {
+        this.top = top;
+        this.bottom = 1;
+    }
     public Fraction(int top, int bottom)
     {
         this.top = top;
         this.bottom = bottom;
     }
 
-    public Fraction(int top)
+    public int GetTop()
     {
-        this.top = top;
-        this.bottom = 1;
+        return top;
+    }
+
+    public void SetTop(int value)
+    {
+        top = value;
+    }
+
+    public int GetBottom()
+    {
+        return bottom;
+    }
+
+    public void SetBottom(int value)
+    {
+        bottom = value;
     }
 
     public string GetFractionString()
     {
         return $"{top}/{bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)top / bottom;
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,12 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Fraction f1 = new Fraction();
-        Console.WriteLine("f1: " + f1.GetFractionString());
+        Console.WriteLine("f1 (default): " + f1.GetFractionString());
 
-        Fraction f2 = new Fraction(6);
-        Console.WriteLine("f2: " + f2.GetFractionString());
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine("f2 (one argument): " + f2.GetFractionString());
 
-        Fraction f3 = new Fraction(6, 7);
-        Console.WriteLine("f3: " + f3.GetFractionString());
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine("f3 (two arguments): " + f3.GetFractionString());
+
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine();
+
+        Console.WriteLine("f3 Top (getter): " + f3.GetTop());
+        Console.WriteLine("f3 Bottom (getter): " + f3.GetBottom());
+
+        f3.SetTop(10);
+        f3.SetBottom(20);
+
+        Console.WriteLine("f3 Updated Top (getter): " + f3.GetTop());
+        Console.WriteLine("f3 Updated Bottom (getter): " + f3.GetBottom());
+
+        Console.WriteLine("f3 Updated Fraction: " + f3.GetFractionString());
     }
+
+
 }
